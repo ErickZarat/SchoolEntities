8019998cf5128db07730a36517a322ec0a6d4b4d
ErickZarat <e.zarat@outlook.com>
2015-09-22 19:51:50 -0600

    mainwindow user control added to tabs

--- EntityDatabase/MainWindow.xaml.vb
+++ EntityDatabase/MainWindow.xaml.vb
@@ -1,12 +1,24 @@
 ﻿Imports Modules.Departments.ViewModels
+Imports Modules.Courses.ViewModels
+Imports Modules.Persons.ViewModel
+Imports Modules.OfficeAssigment.ViewModels
+Imports Modules.OnlineCourses.ViewModels
+Imports Modules.OnsiteCourses.ViewModels
+Imports Modules.StudentsGrade.ViewModel
+
 Class MainWindow
     Sub New()
 
-        ' This call is required by the designer.
         InitializeComponent()
 
-        ' Add any initialization after the InitializeComponent() call.
-        Me.DepartmenstUserControl.MainGrid.DataContext = New DepartmentsViewModel()
+        Me.DepartamenstUserControl.MainGrid.DataContext = New DepartmentsViewModel()
+        Me.CoursesUserControl.MainGrid.DataContext = New CoursesViewModel
+        Me.PersonsUserControl.MainGrid.DataContext = New PersonsViewModel()
+        Me.OfficeAssigList.MainGrid.DataContext = New OfficeAssigmentViewModel()
+        Me.OnlineCourseUserControl.MainGrid.DataContext = New OnlineCoursesViewModel()
+        Me.OnsiteCoursesUserControl.MainGrid.DataContext = New OnsiteCursesViewModel()
+        Me.StudenGradesUserControl.MainGrid.DataContext = New StudentsGradesViewModel()
 
     End Sub
+
 End Class
